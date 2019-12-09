82193d8e59f48e89caa4e53a65468bc2afd84e83
Stephi List <s_list@posteo.de>
2019-12-09 20:36:53 +0100

    animierte Gif in Gui einbinden erste lauffähige Version der Gui Start-Button erzeugt Gif, diese wird in die Gui eingebunden nach Klicken des Restart-Button startet Gui von vorne

--- gui_test_stephi.py
+++ gui_test_stephi.py
@@ -5,114 +5,98 @@
 # sudo apt-get install python-imaging-tk
 # Tk installieren
 # sudo apt-get install python3-tk
-'''
 
 from Tkinter import *
-import ttk
 from PIL import ImageTk, Image
 import os
 import time
 import camera
 
-
-gui = Tk()
-gui.title("Lachmaschuen")
-
+#Gui konfigurieren
+gui1 = Tk()
+gui1.title("Lachmaschuen")
 #Fenstergröße festlegen
-gui.geometry('800x600')
+gui1.geometry('1100x1000')
 
 gif_pfad = ""
 
-# Definieren, was passiert, wenn Buttons geklickt werden
-# Muss vor der Definition des Buttons getan werden!!!
-# Button 1 Definition
-def clicked_button_start():
-    camera.camera_pic()
-    label.pack()
-    gui.after(0, update, 0)
-    # label_gif.pack()
+
+##zum Testen ohne Picamera
+#gif_pfad = "Pfad zum Bild"
+#gif = ImageTk.PhotoImage(Image.open(gif_pfad))
+#label_gif = Label(gui1, image = gif)
+#frames = [ImageTk.PhotoImage(file=gif_pfad, format = 'gif -index %i' %(i)) for i in range(4)]
+
+#Funktionen definieren
+
+##Gui starten
+def start_gui():
+    label_start.pack()
+    button_start.pack()
+    
+def restart_gui():
     label_restart.pack()
     button_restart.pack()
 
-
-## zur Ausgangs-Gui zurückkehren 
-def clicked_button_restart():
-    pass
-
-
-
-
-
-gif_pfad = camera.camera_pic()
-#gif = ImageTk.PhotoImage(Image.open(gif_pfad))
-#label_gif = Label(gui, image = gif)
+##Gif in Gui einbinden
+def gif_einbinden(ind):
+    if gif_is_running: 
+        frame = frames[ind]
+        ind += 1
+        ind = ind%len(frames)
+        #time.sleep(0.15)
+        label_gif.configure(image=frame)
+        gui1.after(100, gif_einbinden, ind)
 
 
-
-frames = [ImageTk.PhotoImage(file=gif_pfad, format = 'gif -index %i' %(i)) for i in range(4)]
-
-
-def update(ind):
-
-    frame = frames[ind]
-    ind += 1
-    ind = ind%len(frames)
-    time.sleep(1)
-    label.configure(image=frame)
-    gui.after(4, update, ind)
+#Funktionen der Buttons definieren
+##Button Start
+def clicked_button_start():
+    #Bilder für Gif erstellen - Kamera-Funktion aufrufen
+    gif_pfad = camera.camera_pic()
+    global frames
+    frames = [PhotoImage(file=gif_pfad,format = 'gif -index %i' %(i)) for i in range(4)]
     
-label = Label(gui)
-label.pack()
-gui.after(0, update, 0)
+    #label_start und button_start löschen
+    label_start.pack_forget()
+    button_start.pack_forget()
+    
+    #Gif einbinden
+    global gif_is_running
+    gif_is_running = True
+    label_gif.pack()
+    gui1.after(0, gif_einbinden, 0)
+    
+    #Label und Button für Restart einblenden
+    restart_gui()
 
+##Button restart
+def clicked_button_restart():
+    #Gif-Darstellung beenden
+    global gif_is_running
+    gif_is_running = False
+    label_gif.pack_forget()
+    
+    #Label restart unf Button restart ausblenden
+    label_restart.pack_forget()
+    button_restart.pack_forget()
+    
+    #Label Start und Button Start einblenden
+    start_gui()
 
-#Label erstellen (im Fenster)
-label_1 = Label(gui, text ="Drücke Start, um die Lachmaschuen zu starten", font = ("Arial", 20))
-label_restart = Label(gui, text ="Bitte hier klicken, um zum Ausgangszustand zurückzukehren", font = ("Arial", 20))
+if __name__ == '__main__':
+    #Button erstellen
+    button_start = Button(gui1, text="Start", command=clicked_button_start, font=("Arial", 20))
+    button_restart = Button(gui1, text = "von vorne", command= clicked_button_restart, font= ("Arial",20))
+    label_gif = Label(gui1)
 
-#Button erstellen
-button_start = Button(gui, text="Start", command=clicked_button_start, font=("Arial", 20))
-button_restart = Button(gui, text = "von vorne", command= clicked_button_restart, font= ("Arial",20))
+    #Label erstellen
+    label_start = Label(gui1, text ="Drücke Start, um die Lachmaschuen zu starten", font = ("Arial", 20))
+    label_restart = Label(gui1, text ="Bitte hier klicken, um zum Ausgangszustand zurückzukehren", font = ("Arial", 20))
 
-##zur Darstellung im Darkmode auf dem Mac
-#button_start = ttk.Button(gui, text="Start", command=clicked_button_start)
-
-
-
-#Button und Labels von oben nach unten erstellen
-label_1.pack()
-button_start.pack()
-
-
-
-
-
-gui.mainloop()
-
-'''
-
-
-
-from Tkinter import *
-import time
-import os
-import camera
-root = Tk()
-
-gif_pfad = camera.camera_pic()
-frames = [PhotoImage(file=gif_pfad,format = 'gif -index %i' %(i)) for i in range(4)]
-
-def update(ind):
-
-    frame = frames[ind]
-    ind += 1
-    ind = ind%len(frames)
-    time.sleep(1)
-    label.configure(image=frame)
-    root.after(4, update, ind)
-label = Label(root)
-label.pack()
-root.after(0, update, 0)
-root.mainloop()
+    #Gui starten
+    start_gui()
+    
+    gui1.mainloop()
 
 
