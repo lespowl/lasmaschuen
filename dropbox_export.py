f948e4c5d6e50771abcf3d700b3c92455114e3e0
Stephi List <s_list@posteo.de>
2019-12-13 14:28:14 +0100

    Dropbox Skript im branch

---
+++ dropbox_export.py
@@ -0,0 +1,19 @@
+import dropbox
+import os
+
+db = dropbox.Dropbox("JOBzoGZ5CMAAAAAAAAAAC_TwWPLaz_udrVSWBC9WDfAGhyCy9P79f8RUC8u0PuHc")
+
+#print('Account infos: ', db.users_get_current_account())
+
+def dropbox_ja(gif_pfad):
+    pic = open(gif_pfad, "rb")
+    gif_name = os.path.basename(gif_pfad)
+    pic_dropbox = "/" + gif_name
+    response = db.files_upload(pic.read(), pic_dropbox)
+    pic.close()
+
+
+if __name__ == '__main__':
+    dropbox_export(gif_pfad)
+    print('uploaded')
+
